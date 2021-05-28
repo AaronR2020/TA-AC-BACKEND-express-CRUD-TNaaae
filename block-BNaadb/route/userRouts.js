b495434bbb51e2bb08b554e8a8c7d2f523784368
AaronR <beingbetter21@gmail.com>
2021-05-29 01:06:23 +0530

    donE

---
+++ block-BNaadb/route/userRouts.js
@@ -0,0 +1,37 @@
+User=require('../model/user');
+express=require('express')
+router=express.Router();
+
+//data to database
+router.post('/',(req,res,next)=>{
+    console.log(req.body);
+    User.create(req.body,(e,d)=>{console.log(e?next(e):d)});
+})
+
+//read data from database
+router.get('/',(req,res)=>{  
+   User.find({},(e,d)=>{
+        e?next(e):console.log(d);
+   })
+   });
+
+//display based on id
+router.get('/:id',(req,res)=>{
+    id=req.params.id
+User.find({_id:id},(e,d)=>{console.log(e?e:d);});
+})
+
+//update
+router.post('/:id/edit',(req,res)=>{
+    id=req.params.id;
+    User.findByIdAndUpdate(id,req.body,(e,d)=>{
+        console.log(e?e:d);
+    })
+})
+
+//delete
+router.get('/:userId/delete',(req,res)=>{
+    id=req.params.userId;
+    User.findByIdAndDelete(id,(e)=>{console.log(e?e:"deleted successfully");});
+});
+module.exports=router;
