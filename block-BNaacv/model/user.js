a15b246d3551bada90e4f0a5e8842be67b770e1a
AaronR <beingbetter21@gmail.com>
2021-05-28 22:15:03 +0530

    e

--- block-BNaacv/model/user.js
+++ block-BNaacv/model/user.js
@@ -2,16 +2,15 @@ express=require('express');
 route=express.Router()
 
 route.get('/',(req,res)=>{
-    req.render('users');
+    users=['Aaron','Baron']
+    req.render('users',{users});
 })
 
 //single user
 route.get('/:id',(req,res)=>{
     id=req.param.id;
-    //val=find({id}) from database
-    //res.render(index,{val})
-})
-
-route.get('/new',(req,res)=>{
-    res.render(`userForm.ejs`);
+    console.log(id);
 });
+
+
+module.exports=route;
